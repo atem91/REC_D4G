832fde1e5d4f728d8fe4c1f1694d23a76c7cda53
Clotilde Saint-Denis <clotilde.saintdenis@gmail.com>
2020-11-04 15:54:37 +0100

    creation of the data treatments

--- D4G/Datas_gestion.py
+++ D4G/Datas_gestion.py
@@ -1,4 +1,9 @@
-import sqlite3
-import requests
+import pandas as pd
+from io import StringIO
+
+#recupere les donnees dont nous allons avoir beosin
+#data = 'Tableau.csv'
+data = pd.read_csv('Tableau.csv')
+print(data)
 
 
