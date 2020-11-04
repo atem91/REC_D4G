282ce808c485a0cb59f4198cc40e91ac0bdf097b
Clotilde Saint-Denis <clotilde.saintdenis@gmail.com>
2020-11-04 16:58:00 +0100

    tab of data and new tab

--- D4G/Datas_gestion.py
+++ D4G/Datas_gestion.py
@@ -5,7 +5,11 @@ from io import StringIO
 #data = 'Tableau.csv'
 #f = open('Tableau.csv')
 
-data = pd.read_csv('Tableau_utf8.csv', error_bad_lines=False)
-print(data)
+given_data_institut = pd.read_csv('Tableau_utf8.csv', error_bad_lines=False)
+print(given_data_institut)
+given_data_insee = pd.read_csv('departement_region.csv', error_bad_lines=False)
+#print(given_data_insee)
 
-
+#selection of department and region, add it on a new tab
+#departement = given_data_insee.iloc[:,3]
+#print(departement)
