e5cbaa667bfff14c9af933d38a74d307d4f965c2
Atem <ricardo.ramos@epita.fr>
2020-11-04 18:14:01 +0100

    Added initalisation of the database

--- D4G/__init__.py
+++ D4G/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 
-from .views import app
-from . import models
+from .views import app, db, init_db
+#from . import models
 
 # Connect sqlalchemy to app
-models.db.init_app(app)
+db.init_app(app)
+
+@app.cli.command()
+def init_db2():
+    init_db()
--- D4G/views.py
+++ D4G/views.py
@@ -1,10 +1,35 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config.from_object('D4G.config')
+db = SQLAlchemy(app)
 
+
+class Zone(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    CityName = db.Column(db.String(300), nullable=False)
+
+    def __init__(self, CityName):
+        self.CityName = CityName
+
+
+
+def init_db():
+    db.drop_all()
+    db.create_all()
+    f = open('D4G/Tableau_utf8.csv')
+    for line in f.readlines():
+        splitted = line.split(';')
+        if splitted[0]:
+            db.session.add(Zone(splitted[0]))
+    f.close()
+    db.session.commit()
 
 @app.route('/')
 def index():
-    return "Hello world!"
+    res = ""
+    for zone in Zone.query.all():
+        res += zone.CityName + '<br/>'
+    return res
