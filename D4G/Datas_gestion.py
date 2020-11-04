59bd51b806792d89ef619bacea08c63cb8bb7294
Clotilde Saint-Denis <clotilde.saintdenis@gmail.com>
2020-11-04 18:26:13 +0100

    new table with dep, reg, cityname

--- D4G/Datas_gestion.py
+++ D4G/Datas_gestion.py
@@ -6,18 +6,9 @@ from io import StringIO
 #f = open('Tableau.csv')
 
 given_data_institut = pd.read_csv('Tableau_utf8.csv', error_bad_lines=False, delimiter=';')
-#print(given_data_institut)
-given_data_insee = pd.read_csv('departement_region.csv', error_bad_lines=False, delimiter=';')
-#print(given_data_insee)
 
-#selection of department and region, add it on a new tab
-departement = given_data_insee.iloc[:,2]
-#print(departement)
-region = given_data_insee.iloc[:,1]
-#print(region)
+new_table = pd.read_csv('departement_region.csv', error_bad_lines=False, delimiter=';', usecols=[1,2,8])
+print(new_table)
+#creation du tableau contenant les reg, dep, ville
 
-new = given_data_institut.sort_values(by='Code Iris')
-print(new)
-#given_data_insee['IRIS'] = given_data_insee['IRIS'].astype(int)
-new_dep = given_data_insee.sort_values(by='IRIS', inplace=True)
-print(new_dep)
+
